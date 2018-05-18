3dafd6b428e6787e064a5797156658fb278a227c
Adailson Aguiar <adailsonacj@live.com>
2018-05-18 10:37:48 -0300

    18/05/2018 10:37

---
+++ AlugaTooC-Sharp/Dao/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlugaTooC_Sharp.Dao
+{
+    public class Usuario
+    {
+        public void adicionaEstado(String nome, String uf, NpgsqlConnection con)
+        {
+            NpgsqlCommand query = new NpgsqlCommand("INSERT INTO public.estados(nome, uf)VALUES ('" + nome + "', '" + uf + "' )", con);
+            query.ExecuteNonQuery();
+        }
+        public String verificaUsuario(String usuario, NpgsqlConnection con)
+        {
+            //String email;
+            NpgsqlCommand query = new NpgsqlCommand("select email from usuario where email = '" + usuario + "'");
+            NpgsqlDataReader retorno = query.ExecuteReader();
+            //retorno.Read();
+            
+            //email = retorno["email"].ToString();
+            
+            return retorno["email"].ToString();
+        }
+    }
+}
